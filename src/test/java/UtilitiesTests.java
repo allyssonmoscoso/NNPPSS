049c465ce3a888ff74c0c900cf019164b5299ff2
SquarePeace@SquarePeace.com <allysson-moscoso500@outlook.com>
2024-03-13 15:06:57 -0400

    Add JUnit dependency and create UtilitiesTests class with test methods

---
+++ src/test/java/UtilitiesTests.java
@@ -0,0 +1,87 @@
+import com.squarepeace.nnppss.Frame;
+import com.squarepeace.nnppss.Utilities;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.Test; // Import the necessary package for the @Test annotation
+import static org.junit.Assert.assertTrue; // Import the necessary package for the assertTrue method
+
+
+public class UtilitiesTests {
+
+
+    //test for createConfigFile method in Utilities class
+    @Test
+    public void testCreateConfigFile() {
+        Utilities utilities = new Utilities();
+        utilities.createConfigFile();
+        File file = new File("config.properties");
+        assertTrue(file.exists());
+    }
+
+    //test for getPSVitaUrl and getPSPUrl methods in Utilities class
+    @Test
+    public void testGetPSVitaUrlAndPSPUrl() {
+        Utilities utilities = new Utilities();
+        utilities.createConfigFile();
+        String pspUrl = utilities.getPSPUrl();
+        String psvitaUrl = utilities.getPSVitaUrl();
+        assertTrue(!pspUrl.isEmpty());
+        assertTrue(!psvitaUrl.isEmpty());
+    }
+
+    //test for getProperty method in Utilities class
+    @Test
+    public void testGetProperty() {
+        Utilities utilities = new Utilities();
+        utilities.createConfigFile();
+        String pspUrl = utilities.getProperty("psp.url");
+        String psvitaUrl = utilities.getProperty("psvita.url");
+        assertTrue(pspUrl.endsWith(".tsv"));
+        assertTrue(psvitaUrl.endsWith(".tsv"));
+    }    
+
+    //test for readTSV method in Utilities class
+    @Test
+    public void testReadTSV() throws FileNotFoundException, IOException {
+        Utilities utilities = new Utilities();
+        assertTrue(utilities.readTSV(utilities.TSV_PSP).getRowCount() > 0);
+        assertTrue(utilities.readTSV(utilities.TSV_VITA).getRowCount() > 0);
+    }
+
+    //test exist db files (db/PSP_GAMES and db/PSV_GAMES) on directory
+    @Test
+    public void testExistDBFiles() {
+        File pspFile = new File("db/PSP_GAMES.tsv");
+        File psvitaFile = new File("db/PSV_GAMES.tsv");
+        assertTrue(pspFile.exists());
+        assertTrue(psvitaFile.exists());
+    }
+    
+    //test for moveFile method in Utilities class
+    @Test
+    public void testMoveFile() throws IOException {
+        Utilities utilities = new Utilities();
+        File file = new File("db/PSP_GAMES.tsv");
+        utilities.moveFile(file.getPath(), "db/PSP_GAMES_COPY.tsv");
+        File fileCopy = new File("db/PSP_GAMES_COPY.tsv");
+        assertTrue(fileCopy.exists());
+        //back to original name
+        utilities.moveFile(fileCopy.getPath(), "db/PSP_GAMES.tsv");
+    }
+
+    //test for convertFileSize method in Utilities class
+    @Test
+    public void testConvertFileSize() {
+        Utilities utilities = new Utilities();
+        String fileSize = utilities.convertFileSize(1024);
+        assertTrue(fileSize.equals("0.0 MiB"));
+    }
+
+    //test for isCommandInstalled method in Utilities class
+    @Test
+    public void testIsCommandInstalled() {
+        Utilities utilities = new Utilities();
+        assertTrue(utilities.isCommandInstalled("java"));
+    }
+}
